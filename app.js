7d64295d85ad6d482e73c0f17982d05742910981
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-02-05 07:44:56 +0300

    js

---
+++ app.js
@@ -0,0 +1,167 @@
+
+
+
+
+
+
+// 1-mashq 
+
+
+// let son = 43211234
+
+// son = son.toString()
+// let jamison= "";
+// let i = son.length - 1;
+// while (0<=i) {
+//     jamison += son[i]
+//     i--;
+// }
+// console.log(+jamison == +son)
+
+// 2-mashq  
+
+// let son = 8275
+// son = son.toString()
+// let i = 0
+// while (i < son.length) {
+//     if (son[i] % 2 != 0){
+//         console.log(true)
+//     break;
+        
+//     }
+// console.log(i);
+
+//     i++;
+// }
+// console.log(son)
+
+
+
+// 3-mashq
+
+
+// let narx = 18500
+
+// for ( let i=1; i<= 10; i++) {
+//    console.log(`${i/10}kg = ${(narx * (i/10)).toFixed()} som`)
+// }
+
+
+// 4-mashq 
+
+// let a = 5
+// let b = 8
+// let c = []
+
+// for (let i = a; i <= b; i++) {
+//    c.push(i)
+// }   
+
+// console.log(c)
+
+// 5-mashq
+
+// let narx = 18000
+
+// for ( let i= 1; i <= 10;i++) {
+// console.log(`${i} kg = ${(i *narx )} so'm `);
+// }
+
+
+// 6- mashq
+
+// let narx = 5000
+
+// for ( let i= 5; i <= 10;i++) {
+// console.log(`${i/5} kg = ${(narx * (i/2) )} so'm `);
+// }
+
+// 7-mashq 
+
+
+// let a = 10 
+// let b = 20
+// let d = 0
+
+// for (let i = a; i <= b; i++) {
+//    if (i += i % 2) {
+//     d += i
+//    }
+// }
+// console.log (d)
+
+
+
+
+
+// 8-mashq
+
+
+// let a = 10 
+// let b = 20
+// let d = 0
+
+// for (let i = a; i <= b; i++) {
+//    if (i += i % 2) {
+//     d += i * 2
+//    }
+// }
+// console.log (d)
+  
+
+
+
+// 9 -mashq 
+
+// let a = 10 
+// let b = 20
+// let d = 0
+
+// for (let i = a; i <= b; i++) {
+//    if (i += i % 2) {
+//     d += i *i
+//    }
+// }
+// console.log (d)
+
+
+// ***********************  10 - mashq *******************
+
+// let n = 5 
+// let counter = 0
+// for (let i = 0; i <= n;i++) {
+//    counter += (n + i) ** 2
+// }
+// console.log(counter)
+
+// ***********************  11 - mashq *******************
+  
+// let son = 5
+// let pover = son
+// let sum = 0
+// for(let i = 1; i<=son;i++) {
+//    sum += i ** pover
+//    pover--
+// }
+// console.log(sum)
+
+// ***********************  12 - mashq *******************
+//  let sum = ""
+//  for(let i = 1; i <= 5;i++) {
+
+//    for(let k = 1; k <= i;k++){
+//       sum += `${i }`
+//    }
+//  }
+// console.log(sum)
+
+// ***********************  13 - mashq *******************
+
+// let n = 10
+// let num = ""
+// for(let i = 1; i <= n;i++) {
+//     if(n % i == 0) {
+//         num += `${i }`
+//     }
+// }
+// console.log(num.trimEnd())
